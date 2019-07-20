344a7fd74927977e35743efa901e44c98be43485
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2019-07-20 13:31:16 -0500

    Create Pasajero.java

---
+++ LineaDeAutobuses/src/Pasajero.java
@@ -0,0 +1,35 @@
+
+public class Pasajero {
+    private String nombre;
+    private String apellido;
+
+    public Pasajero() {
+
+    }
+
+    public Pasajero(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Pasajero{" + "nombre='" + nombre + '\'' + ", apellido='" + apellido + '\'' + '}';
+    }
+}
