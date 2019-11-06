eb1b766c7a2e1c8b6f943e68ad76fdc5c76f72a5
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2019-11-06 09:01:54 -0600

    Create exercise_2.java

---
+++ Aprende_Java_con_ejercicios/codes/com/capitulo_1/exercise_2.java
@@ -0,0 +1,18 @@
+
+package com.capitulo_1;
+
+/**
+ *Show my name and my information on screen.
+ *
+ * @author Jorge Alberto Gomez Gomez.
+ *
+ * December, 21 del 2018, 09:14 pm.
+ */
+
+public class exercise_2 {
+    public static void main(String[] args) {
+        System.out.println("My name is: Jorge Alberto Gomez Gomez");
+        System.out.println("My address is: Street 3 West 518 / 5 and 7 South, San Pedro Cholula, Puebla");
+        System.out.println("My phone number is: +52 1 5540 878 933");
+    }
+}
