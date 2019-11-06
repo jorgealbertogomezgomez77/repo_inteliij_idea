fc8705df80b109914d10f41a1e00b6e1950ab015
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2019-11-06 09:01:57 -0600

    Create Hello_World.java

---
+++ Aprende_Java_con_ejercicios/codes/com/capitulo_1/Hello_World.java
@@ -0,0 +1,16 @@
+
+package com.capitulo_1;
+
+/**
+ * Show on screen the phrase "¡Hola Mundo ... !!!."
+ *
+ * @author Jorge Alberto Gomez Gomez.
+ *
+ * December, 21 del 2018, 06:42 pm.
+ */
+
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("¡¡¡Hello World...!!!");
+    }
+}
