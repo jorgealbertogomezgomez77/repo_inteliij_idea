287279a9edc3dd8d723645d4d2bce979c4bf1642
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2019-11-06 09:01:51 -0600

    Create exercise_1.java

---
+++ Aprende_Java_con_ejercicios/codes/com/capitulo_1/exercise_1.java
@@ -0,0 +1,16 @@
+
+package com.capitulo_1;
+
+/**
+ *Show my name on screen.
+ *
+ * @author Jorge Alberto Gomez Gomez.
+ *
+ * December, 21 del 2018, 09:14 pm.
+ */
+
+public class exercise_1 {
+    public static void main(String[] args) {
+        System.out.println("My name is: Jorge Alberto Gomez Gomez");
+    }
+}
