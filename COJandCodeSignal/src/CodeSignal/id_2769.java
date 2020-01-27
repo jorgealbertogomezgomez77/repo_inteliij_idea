b32ad60be72240b0b92ba1c13670995152e11279
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2020-01-26 23:59:50 -0600

    Create id_2769.java

---
+++ COJandCodeSignal/src/CodeSignal/id_2769.java
@@ -0,0 +1,13 @@
+package CodeSignal;
+
+import java.util.Scanner;
+
+public class id_2769 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println((i + 1) + ": I am participating in the Engineer's day.");
+        }
+    }
+}
