cba0863e65ddfca90af76256f92309a8d8d0fb37
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2020-01-30 00:08:34 -0600

    Create Memory_2.java

---
+++ com.lwdp.www/Maestria_en_videojuegos/src/videojuegos/maestria/java/Memory/Memory_2.java
@@ -0,0 +1,9 @@
+package videojuegos.maestria.java.Memory;
+
+import javax.swing.*;
+
+public class Memory_2 {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button5;
+}
