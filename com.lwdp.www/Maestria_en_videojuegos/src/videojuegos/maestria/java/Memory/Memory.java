d543cc40d28f780dec37224b03ddbcf280a8c543
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2020-01-30 00:08:29 -0600

    Create Memory.java

---
+++ com.lwdp.www/Maestria_en_videojuegos/src/videojuegos/maestria/java/Memory/Memory.java
@@ -0,0 +1,19 @@
+package videojuegos.maestria.java.Memory;
+
+import javax.swing.*;
+
+public class Memory {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button8;
+    private JButton button9;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
